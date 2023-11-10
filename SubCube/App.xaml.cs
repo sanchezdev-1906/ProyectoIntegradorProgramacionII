2be9ecce0b185383d6c00ba89c7ee6bba221ac00
Joel Sanchez <joelalejandro.development@gmail.com>
2023-11-10 01:51:29 -0400

    primer commit

---
+++ SubCube/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SubCube
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public string ActualTheme = "Light";
+        public void ChangeTheme(Uri dictionaryUri)
+        {
+            ResourceDictionary newDictionary = new ResourceDictionary();
+            newDictionary.Source = dictionaryUri;
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(newDictionary);
+        }
+        
+    }
+}
---
+++ SubCube/Pages/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SubCube.Pages
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (((App)Application.Current).ActualTheme == "Light")
+            {
+                ((App)Application.Current).ChangeTheme(new Uri("Themes/ThemeDark.xaml", UriKind.Relative));
+                ((App)Application.Current).ActualTheme = "Dark";
+            }
+            else if (((App)Application.Current).ActualTheme == "Dark")
+            {
+                ((App)Application.Current).ChangeTheme(new Uri("Themes/ThemeLight.xaml", UriKind.Relative));
+                ((App)Application.Current).ActualTheme = "Light";
+            }
+        }
+    }
+}
